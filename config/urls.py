e6cc58e043f2473fea0436ea4ec4d9f3844d4e68
fvilpaz <fvilpaz@gmail.com>
2026-08-02 03:42:57 +0200

    tmp: reset password utility

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from core.views import reset_tmp_x9k2
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("reset-tmp-x9k2/", reset_tmp_x9k2, name="reset_tmp"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", include("core.urls")),
